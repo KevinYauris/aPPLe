11de6ab2d9aab929f2ef5cd462eade46137c55e1
raihannurr <raihannurr@gmail.com>
2016-02-15 22:39:54 +0700

    revisi statistik umum

--- statumum.php
+++ statumum.php
@@ -26,20 +26,17 @@
 
 			join
 
-			(select count(*) as nlain from alat natural join peminjaman 
-
-			where lower(kategori)='lain') as d
-
-			)			
+			(select count(*) as nlain from alat natural join peminjaman) as d
+			)						
 			"));
 
 			$chart = new PieChart();	
-
+			$nlain = $count['nlain']-$count['nlaptop']-$count['nproyektor']-$count['nsound'];
 			$dataSet = new XYDataSet();
 			$dataSet->addPoint(new Point("Sound (". $count['nsound'] . ")", $count['nsound']));
 			$dataSet->addPoint(new Point("Proyektor (". $count['nproyektor'] . ")", $count['nproyektor']));
 			$dataSet->addPoint(new Point("Laptop (". $count['nlaptop'] . ")", $count['nlaptop']));
-			$dataSet->addPoint(new Point("Misc (". $count['nlain'] . ")", $count['nlain']));
+			$dataSet->addPoint(new Point("Misc (". $nlain . ")", $nlain));
 			$chart->setDataSet($dataSet);	
 			
 			$chart->setTitle("Statistik Umum Peminjaman Barang");
